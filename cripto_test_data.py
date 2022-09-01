3843e0ed79c93fff8c42f4f8b9ab5dcb90647047
Roman Lazovskiy <r.v.lazovskiy@gmail.com>
2022-09-01 16:03:03 +0500

    del private information

--- cripto_test_data.py
+++ cripto_test_data.py
@@ -1,2 +1,2 @@
-api_key_test =  'WX2Rso4QuhIHACD7TeQe3pQGf9GzgtV157mwJBiYFYm4TqIgs1xMPx3BHHdEpwGx'
-secret_key_test = '8lRGC2vGcFfEzy2f5EXdIPchdihq4AWw8M0fXpcTzePvYpcIU0wUmpn2qd7rYRm9'
+api_key_test =  ''
+secret_key_test = ''
